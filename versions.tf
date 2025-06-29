04055fe835490a6d05573587a74181548ec064e0
Wagner Torres <wav_torres@hotmail.com>
2025-06-29 13:29:09 -0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,1 +1,17 @@
+# Define as versões mínimas necessárias para o Terraform e provedores
+terraform {
+  required_version = ">= 1.0.0"
 
+  required_providers {
+    # Usamos o provedor 'null' para gerenciar a execução de comandos remotos
+    # e o provedor 'local' para interagir com arquivos locais.
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
